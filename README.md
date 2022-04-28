5bb0b44bb33bfce0143841beeda48e4cd78ae431
Mikołaj Matuszny <mikolaj.matuszny@nordcloud.com>
2022-04-28 09:10:05 +0200

    Revert "initial commit"
    This reverts commit 971e4ebf8fa03044cd32061ca81a0d5a2608b8db.

--- README.md
+++
@@ -1,2 +0,0 @@
-# datadog-users-management
-Script to modify Datadog users based on file containing APP and API keys.
--- user_management/slack_users/user.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2021 Nordcloud Oy or its affiliates. All Rights Reserved.
-class User:
-    def __init__(self, id, name, email):
-        self.id = id
-        self.name = name
-        self.email = email
-
-    def __str__(self):
-        return "{0} {1} {2}".format(self.id, self.name, self.email)
